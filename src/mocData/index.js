dc6abc4a89316aa1ec6a2b8dfe99c0b9ee64bdb2
yynyds <yarik1120@gmail.com>
2022-01-05 00:40:44 +0200

    init template

---
+++ src/mocData/index.js
@@ -0,0 +1,64 @@
+export const payloadData = [
+    {
+        groupId: 1,
+        label: '1',
+        questions: [
+            {
+                id: 11,
+                health: '0',
+                symptoms: 'Acid foods upset'
+            },
+            {
+                id: 12,
+                health: '0',
+                symptoms: 'Get chilled often'
+            },
+            {
+                id: 13,
+                health: '0',
+                symptoms: '"Lump" in throat'
+            },
+            {
+                id: 14,
+                health: '0',
+                symptoms: 'Dry mouth - eyes - nose'
+            },
+            {
+                id: 15,
+                health: '0',
+                symptoms: 'Pulse speeds after meals'
+            }
+        ]
+    },
+    {
+        groupId: 2,
+        label: '2',
+        questions: [
+            {
+                id: 21,
+                health: '0',
+                symptoms: 'Acid foods upset'
+            },
+            {
+                id: 22,
+                health: '0',
+                symptoms: '"Lump" in throat'
+            },
+            {
+                id: 23,
+                health: '0',
+                symptoms: 'Get chilled often'
+            },
+            {
+                id: 24,
+                health: '0',
+                symptoms: 'Pulse speeds after meals'
+            },
+            {
+                id: 25,
+                health: '0',
+                symptoms: 'Dry mouth - eyes - nose'
+            }
+        ]
+    },
+];
